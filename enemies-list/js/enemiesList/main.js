8f58c0bed6d243f5dd627970126d9e953dc578d0
Micaela Colpi <micaelacolpi@gmail.com>
2021-05-27 10:26:19 -0500

    enemies list

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,8 @@
 // Put your code here
+console.log("My Enemies List!")
+console.log("------------------")
+const listOfEnemies = ["Joshua Flowers (Really, really dislike!)", "Darth Vader", "Betty Rudelady (Really, really dislike!)", "Leon Peck"]
+for (let enemie of listOfEnemies) {
+    console.log(enemie)
+
+};
