7aa2c615de30b26360fc8b4a59cd56620bd575a1
Micaela Colpi <micaelacolpi@gmail.com>
2021-05-27 10:11:38 -0500

    place-names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,12 @@
 // Put your code here
+
+console.log("All Place Names")
+const namesOfCities = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+for (let name of namesOfCities) {
+    console.log(` ${name}`);
+}
+console.log("`The` Place Names")
+let namesWithThe = namesOfCities.filter(name => name.includes("The"))
+for (let name of namesWithThe) { console.log(`${name}`) }
+
