b35a9c89e127dfc99e9ddc436fea2550dfde205f
Micaela Colpi <micaelacolpi@gmail.com>
2021-06-10 15:49:09 -0500

    Rolling dice

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,39 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+const roll = () => {
+    const dieValue = Math.ceil(Math.random() * 7);
+    return dieValue
+}
+
+// for (int i = 0; i < 10; i++) {
+//     const die1 = roll();
+//     const die2 = roll();
+
+//     const message = "${die1}"
+// }
+
+const round = () => {
+    const die1 = roll();
+    const die2 = roll();
+    const total = die1 + die2;
+    if (die1 === die2) {
+        console.log('? + ? = ' + total + ' DOUBLES!')
+    } else {
+        console.log('? + ? = ' + total)
+    }
+
+}
+
+round();
+round();
+round();
+round();
+round();
+round();
+round();
+round();
+round();
+round();
